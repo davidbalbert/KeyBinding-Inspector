9f57278afa13f83923c1985281104e05f604b90c
David Albert <davidbalbert@gmail.com>
2023-12-11 16:44:31 -0500

    Initial commit

---
+++ KeyBinding Inspector/KeyBindingInspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyBindingInspectorApp.swift
+//  KeyBinding Inspector
+//
+//  Created by David Albert on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct KeyBindingInspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeyBinding InspectorUITests/KeyBinding_InspectorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KeyBinding_InspectorUITestsLaunchTests.swift
+//  KeyBinding InspectorUITests
+//
+//  Created by David Albert on 12/11/23.
+//
+
+import XCTest
+
+final class KeyBinding_InspectorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

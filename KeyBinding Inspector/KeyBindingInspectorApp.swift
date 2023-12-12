b45be39bedd048feaeb29a90cbbec87d19d68eec
David Albert <davidbalbert@gmail.com>
2023-12-11 19:14:09 -0500

    Add “Open System Key Bindings” menu item

--- KeyBinding Inspector/KeyBindingInspectorApp.swift
+++ KeyBinding Inspector/KeyBindingInspectorApp.swift
@@ -10,9 +10,21 @@ import UniformTypeIdentifiers
 
 @main
 struct KeyBindingInspectorApp: App {
+    @Environment(\.openDocument) var openDocument
+
     var body: some Scene {
         DocumentGroup(viewing: KeyBindingsDocument.self) { configuration in
             KeyBindingsView(document: configuration.document)
+        }
+        .commands {
+            CommandGroup(after: .newItem) {
+                Button("Open System Key Bindings") {
+                    let url = URL(fileURLWithPath: "/System/Library/Frameworks/AppKit.framework/Resources/StandardKeyBinding.dict")
+                    Task {
+                        try await openDocument(at: url)
+                    }
+                }
+            }
         }
     }
 }

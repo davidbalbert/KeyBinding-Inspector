6b5d0f83ee107b797d56021f12e133fd8a3be780
David Albert <davidbalbert@gmail.com>
2023-12-18 15:30:51 -0500

    Help menu opens GitHub repo

--- KeyBinding Inspector/AppDelegate.swift
+++ KeyBinding Inspector/AppDelegate.swift
@@ -14,10 +14,29 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var updaterState: UpdaterState = UpdaterState()
     var updaterWindow: NSWindow?
 
+    let repositoryURL = URL(string: "https://github.com/davidbalbert/KeyBinding-Inspector")!
+    let systemKeyBindingsURL = URL(fileURLWithPath: "/System/Library/Frameworks/AppKit.framework/Resources/StandardKeyBinding.dict")
+
+    var userKeyBindingsURL: URL? {
+        guard let passInfo = getpwuid(getuid()) else {
+            return nil
+        }
+        let homeDir = String(cString: passInfo.pointee.pw_dir)
+        return URL(fileURLWithPath: homeDir + "/Library/KeyBindings/DefaultKeyBinding.dict")
+    }
+
+    var userKeyBindingsExists: Bool {
+        guard let path = userKeyBindingsURL else {
+            return false
+        }
+        return FileManager.default.fileExists(atPath: path.path)
+    }
+
+
     func applicationDidFinishLaunching(_ notification: Notification) {
         Task {
             if NSDocumentController.shared.documents.isEmpty {
-                open(url: systemKeyBindingsURL)
+                open(fileURL: systemKeyBindingsURL)
             }
         }
     }
@@ -49,40 +68,46 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         updaterWindow = w
     }
 
-    let systemKeyBindingsURL = URL(fileURLWithPath: "/System/Library/Frameworks/AppKit.framework/Resources/StandardKeyBinding.dict")
-
-    var userKeyBindingsURL: URL? {
-        guard let passInfo = getpwuid(getuid()) else {
-            return nil
-        }
-        let homeDir = String(cString: passInfo.pointee.pw_dir)
-        return URL(fileURLWithPath: homeDir + "/Library/KeyBindings/DefaultKeyBinding.dict")
-    }
-
-    var userKeyBindingsExists: Bool {
-        guard let path = userKeyBindingsURL else {
-            return false
-        }
-        return FileManager.default.fileExists(atPath: path.path)
-    }
-
     @objc func openSystemKeyBindings(_ sender: Any?) {
-        open(url: systemKeyBindingsURL)
+        open(fileURL: systemKeyBindingsURL)
     }
 
     @objc func openUserKeyBindings(_ sender: Any?) {
         if let userKeyBindingsURL {
-            open(url: userKeyBindingsURL)
+            open(fileURL: userKeyBindingsURL)
         }
     }
 
-    func open(url: URL) {
+    func open(fileURL: URL) {
         Task {
             do {
-                let _ = try await NSDocumentController.shared.openDocument(withContentsOf: url, display: true)
+                let _ = try await NSDocumentController.shared.openDocument(withContentsOf: fileURL, display: true)
             } catch {
                 let alert = NSAlert(error: error)
                 alert.runModal()
+            }
+        }
+    }
+
+    @objc func openReadme(_ sender: Any?) {
+        // This gives warnings to do with concurrency. Not sure why given that everything's
+        // running on MainActor, but let's do it the old fashioned way.
+
+        // Task {
+        //     do {
+        //         let _ = try await NSWorkspace.shared.open(helpURL, configuration: .init())
+        //     } catch {
+        //         let alert = NSAlert(error: error)
+        //         alert.runModal()
+        //     }
+        // }
+
+        NSWorkspace.shared.open(repositoryURL, configuration: .init()) { _, error in
+            if let error {
+                DispatchQueue.main.async {
+                    let alert = NSAlert(error: error)
+                    alert.runModal()
+                }
             }
         }
     }
